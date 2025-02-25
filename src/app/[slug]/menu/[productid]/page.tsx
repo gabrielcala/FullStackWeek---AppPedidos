b5664948c140f103a15dae2c9ad5e5ced34dcfba
Gabriel Gustavo Calá <calagabriel6@gmail.com>
2025-02-25 10:45:04 -0300

    feat: tela de produtos concluida

--- src/app/[slug]/menu/[productid]/page.tsx
+++ src/app/[slug]/menu/[productid]/page.tsx
@@ -1,27 +1,40 @@
-import { db } from '@/lib/prisma';
-import { notFound } from 'next/navigation';
-import React from 'react'
-import ProductHeader from './components/product-header';
+import { notFound } from "next/navigation";
+
+import { db } from "@/lib/prisma";
+
+import ProductDetails from "./components/product-details";
+import ProductHeader from "./components/product-header";
 
 interface ProductPageProps {
-  params: Promise<{ slug: string, productid: string }>;
+  params: Promise<{ slug: string; productId: string }>;
 }
 
 const ProductPage = async ({ params }: ProductPageProps) => {
-  const { slug, productid } = await params;
-  const product = await db.product.findUnique({ where: { id: productid } });
+  const { slug, productId } = await params;
+  const product = await db.product.findUnique({
+    where: { id: productId },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+          avatarImageUrl: true,
+          slug: true,
+        },
+      },
+    },
+  });
   if (!product) {
-    return notFound()
+    return notFound();
+  }
+  if (product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+    return notFound();
   }
   return (
-    <>
-  <ProductHeader product={product} />
-    <h1>Product Page</h1>
-    {slug}
-    {productid}
-    </>
-  
-  )
-}
+    <div className="flex h-full flex-col">
+      <ProductHeader product={product} />
+      <ProductDetails product={product} />
+    </div>
+  );
+};
 
-export default ProductPage
+export default ProductPage;
